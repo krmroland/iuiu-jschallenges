a8da1d4732b0a27ee512743e628b2b5936f204db
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2018-09-20 00:45:55 +0300

    Refactor solution for question 10

--- questions/changeCaseByLength.js
+++ questions/changeCaseByLength.js
@@ -5,8 +5,9 @@
  * @return {String} str
  */
 const changeCaseByLength = str => {
-    if (str.length < 3) return str.toUpperCase();
-    return str.substring(0, 3).toLowerCase() + str.substring(3).toUpperCase();
+    return str.length < 3
+        ? str.toUpperCase()
+        : str.substring(0, 3).toLowerCase() + str.substring(3).toUpperCase();
 };
 
 module.exports = changeCaseByLength;
